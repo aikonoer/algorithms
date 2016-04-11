d7a83585b2e2da782f8cf107839ee74515297477
aikonoer <brianmomongan@live.com.au>
2016-04-12 01:34:57 +1000

    bubble sort

--- main/scala/sorting/bubbleSort/BubbleSortImpl.scala
+++ main/scala/sorting/bubbleSort/BubbleSortImpl.scala
@@ -1,8 +1,46 @@
 package sorting.bubbleSort
 
+import scala.annotation.tailrec
+
 /**
   * Created by brianmomongan on 4/11/16.
   */
-class BubbleSortImpl {
 
+class BubbleSortImpl[T <: Int] {
+
+  def sortList(list: List[T]): List[T] = {
+
+    val listLen = list.length
+
+    def swap(list: List[T], from: Int): List[T] = {
+      val swapped = list(from + 1) :: list(from) :: Nil
+      val first = if (from == 0) List() else list.take(from)
+      val last = if (from + 1 == list.length - 1) List() else list.drop(from + 2)
+      first ++ swapped ++ last
+    }
+
+    @tailrec
+    def loopToSort(list: List[T], current: Int = 0, it: Int): List[T] = {
+      if (current < it) {
+        if (list(current) > list(current + 1)) loopToSort(swap(list, current), current + 1, it)
+        else loopToSort(list, current + 1, it)
+      }
+      else list
+    }
+
+    @tailrec
+    def loop(list: List[T], it: Int = listLen): List[T] = {
+      if (0 < it) loop(loopToSort(list, 0, it - 1), it - 1)
+      else list
+    }
+    loop(list)
+  }
 }
+
+object Main extends App {
+
+  val bubble = new BubbleSortImpl[Int]
+  val list: List[Int] = 5 :: 2 :: 0 :: 9 :: 1 :: 2 :: 4 :: Nil
+  println(bubble.sortList(list))
+}
+
---
+++ main/scala/sorting/bubbleSort/test.sc
@@ -0,0 +1,3 @@
+val list: List[Int] = (1 to 10).toList
+
+list(5) :: list(4) :: Nil
